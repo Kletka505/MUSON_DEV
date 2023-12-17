9155ece1ec2ed126fa0a129c7eb92a5997779290
Kletka505 <ilja.bazhanov@gmail.com>
2023-12-17 18:52:33 +0300

    TESTS

--- test_main.py
+++ test_main.py
@@ -10,14 +10,4 @@ def test_docs_main():
     assert response.status_code == 200
 
 
-def test_catalog_main():
-    response = client.get("/catalog")
-    assert response.status_code == 200
-
-    
-def test_account_main():
-    response = client.get("/account")
-    assert response.status_code == 200
-
-
 
